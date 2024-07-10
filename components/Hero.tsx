8eec616ea66e4d51d71d3ced95b4f4b2b07ebd4a
MaiEdward <edwmai1813@gmail.com>
2024-07-09 18:03:28 -0700

    bento box components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
-import MagicButton from './ui/MagicButton';
+import MagicButton from './MagicButton';
 import { FaLocationArrow } from 'react-icons/fa6';
 
 const Hero = () => {
@@ -39,7 +39,7 @@ const Hero = () => {
                 </p>
                 <a href='#about'>
                     <MagicButton 
-                    title="Show my work"
+                    title="Read More"
                     icon = {<FaLocationArrow/>}
                     position='right'
                     />
