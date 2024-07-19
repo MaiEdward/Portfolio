c89a67c6d8c2baf906a05dfc7a5153e34f3b6ede
MaiEdward <edwmai1813@gmail.com>
2024-07-19 14:49:48 -0700

    fix img alt in Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className='mt-5 w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                         <a href={profile.link} target='blank'>
-                            <img src={profile.img} alt={profile.id}
+                            <img src={profile.img} alt="icons"
                             width={20}
                             height={20}
                             />
