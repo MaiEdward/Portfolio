6e454151ed675450fc1e3b4826af0f278a16ffb2
MaiEdward <edwmai1813@gmail.com>
2024-07-22 16:28:23 -0700

    change description of web

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 import Footer from "../components/Footer";
 
 export default function Home() {
